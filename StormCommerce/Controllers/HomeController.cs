6d0440a71f9790f9dbb827a1426a81c5887fedf8
TayfunF <firtinatayfun08@gmail.com>
2022-08-14 15:29:14 +0300

    Kategori Listesi, Ürün Resimleri Ekleme,Ürün Listeleme ve Detay Sayfaları,StyleCSS düzenlemeleri

--- StormCommerce/Controllers/HomeController.cs
+++ StormCommerce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using StormCommerce.Models;
+using StormCommerce.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,17 +15,51 @@ namespace StormCommerce.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View(_context.Products.Where(x => x.IsHome && x.IsApproved).ToList());
+            var Urunler = _context.Products.Where(p => p.IsHome && p.IsApproved).Select(p => new ProductVM()
+            {
+                Id = p.Id,
+                Name = p.Name.Length > 50 ? p.Name.Substring(0, 47) + "..." : p.Name,
+                Description = p.Description.Length > 50 ? p.Description.Substring(0, 47) + "..." : p.Description,
+                Price = p.Price,
+                Stock = p.Stock,
+                Image = p.Image == null ? "resim-yok.png" : p.Image,
+                CategoryId = p.CategoryId,
+            }).ToList();
+
+            return View(Urunler);
         }
 
         public ActionResult ProductDetails(int id)
         {
-            return View(_context.Products.Where(x=>x.Id == id).FirstOrDefault());
+            return View(_context.Products.Where(x => x.Id == id).FirstOrDefault());
         }
 
-        public ActionResult ProductList()
+        public ActionResult ProductList(int? id)
         {
-            return View(_context.Products.Where(x => x.IsApproved).ToList());
+            var Urunler = _context.Products.Where(p => p.IsApproved).Select(p => new ProductVM
+            {
+                Id = p.Id,
+                Name = p.Name.Length > 50 ? p.Name.Substring(0, 47) + "..." : p.Name,
+                Description = p.Description.Length > 50 ? p.Description.Substring(0, 47) + "..." : p.Description,
+                Price = p.Price,
+                Stock = p.Stock,
+                Image = p.Image,
+                CategoryId = p.CategoryId,
+            }).AsQueryable();
+
+            if (id != null)
+            {
+                Urunler = Urunler.Where(p => p.CategoryId == id);
+            }
+
+            ViewBag.ProductCount = Urunler.Count();
+
+            return View(Urunler.ToList());
+        }
+
+        public PartialViewResult GetCategoryList()
+        {
+            return PartialView(_context.Categories.ToList());
         }
     }
 }
---
+++ StormCommerce/Migrations/202208140715572_Init2.cs
@@ -0,0 +1,18 @@
+﻿namespace StormCommerce.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Image", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "Image");
+        }
+    }
+}
