afb4141064c4dbefbc93330d10f842fa1708c3a7
TayfunF <tayfunbjk08@hotmail.com>
2022-08-31 22:42:18 +0300

    Sepete Ekleme İşlemleri Bu Committe

---
+++ StormCommerce/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using StormCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StormCommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        AppDbContext _context = new AppDbContext();
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            //Cart cart = (Cart)Session["Cart"];
+            var cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+
+        public PartialViewResult _SummaryPartial()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
---
+++ StormCommerce/Models/Cart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StormCommerce.Models
+{
+    public class Cart
+    {
+        private List<CartLine> _CartLines = new List<CartLine>();
+        public List<CartLine> CartLines { get { return _CartLines; } }
+
+        public void AddProduct(Product product, int quantity)
+        {
+            var Line = _CartLines.Where(x => x.Product.Id == product.Id).FirstOrDefault();
+
+            if (Line == null)
+            {
+                _CartLines.Add(new CartLine() { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                Line.Quantity += quantity;
+            }
+        }
+        public void DeleteProduct(Product product)
+        {
+            _CartLines.RemoveAll(x => x.Product.Id == product.Id);
+        }
+        public double TotalProduct()
+        {
+            return _CartLines.Sum(x => x.Product.Price * x.Quantity);
+        }
+        public void Clear()
+        {
+            _CartLines.Clear();
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
